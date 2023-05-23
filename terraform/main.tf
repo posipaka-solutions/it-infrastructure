138a274211bc1ae43da75cb325006a1d93dd9369
DarkNeon135 <baryn72@gmail.com>
2023-05-24 01:32:33 +0200

    added egress policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,12 @@ resource "aws_security_group" "OV_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 }
 
 
