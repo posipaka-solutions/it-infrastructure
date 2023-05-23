4fdc331be8092e4a4ad6a6a15bba641949b83d12
DarkNeon135 <baryn72@gmail.com>
2023-05-24 01:54:29 +0200

    edited ingress policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "OV_sg" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
