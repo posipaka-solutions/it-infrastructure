858ac7961631b73d47e89a261b432fad4d363521
DarkNeon135 <baryn72@gmail.com>
2023-05-24 00:39:47 +0200

    edited instance type version

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "OV_sg" {
 
 resource "aws_instance" "it_lab_2" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   vpc_security_group_ids = [aws_security_group.OV_sg.id]
 
